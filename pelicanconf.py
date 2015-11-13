68f42f72957368e8285e6ac39164d2e462f66148
ygilad <yanivdll@gmail.com>
2015-11-13 12:45:40 -0500

    added disqus

--- pelicanconf.py
+++ pelicanconf.py
@@ -69,18 +69,18 @@ LOAD_CONTENT_CACHE = False
 JINJA_EXTENSIONS = ['webassets.ext.jinja2.AssetsExtension',]
 
 # THEME = '../pelican-themes/aboutwilson'
-THEME = '../pelican-themes/prodissues_simple'
+THEME = 'themes/simple'
 ASSET_URL = '/static/'
 
 PLUGIN_PATHS = ['../pelican-plugins']
-PLUGINS = ['assets', 'org_reader', 'pelican_comment_system']
+PLUGINS = ['assets', 'org_reader']
 
 # Org Reader plugin
 ORG_READER_EMACS_LOCATION = "/Applications/Emacs.app/Contents/MacOS/Emacs"
 ORG_READER_EMACS_SETTINGS = "~/.emacs.d/init.el"
 
-# Commenting system
-PELICAN_COMMENT_SYSTEM = True
+# Commenting systems
+# PELICAN_COMMENT_SYSTEM = True
 
 # For testing, to make sure traffic is tracked in ga. Should be commented after testing.
 #GOOGLE_ANALYTICS = "UA-60771520-1"
--- publishconf.py
+++ publishconf.py
@@ -21,5 +21,5 @@ DELETE_OUTPUT_DIRECTORY = True
 
 # Following items are often useful when publishing
 
-#DISQUS_SITENAME = ""
+DISQUS_SITENAME = 'prodissues'
 GOOGLE_ANALYTICS = "UA-60771520-1"
